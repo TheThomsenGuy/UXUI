50825c7886178b51f3731c25a29aba64995f5c8e
TheThomsenGuy <jommamas@gmail.com>
2021-04-21 09:29:41 +0200

    Thomas
    Tablet skærm

---
+++ Tablet - Thom/js/script.js
@@ -0,0 +1,6 @@
+
+/*Her afspilles animationen på tablet, så check ind er confirmed*/
+
+document.getElementById('button').addEventListener("click", function(){
+  document.getElementById('indhold').style.animationPlayState = "Running";
+});
